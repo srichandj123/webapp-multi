8c4347e4b90b7d21710927aacf33b0e7ae1ba71f
srichandj123 <srichandbec@gmail.com>
2025-02-24 03:06:13 +0530

    Fix middle

--- middleware.tf
+++ middleware.tf
@@ -1,43 +1,23 @@
+#Middleware App SVC Plan
 module "mid-webapsvcplan" {
   source     = "./modules/app-svc-plan"
-  apsvc_name = mid_apsvc_plan
+  apsvc_name = var.mid_apsvc_plan
   rg_name    = module.rg.rg_name
   location   = module.rg.location
   os_type    = "Windows"
   sku_name   = "P0v3"
 }
-
+#Middleware WebApp
 module "mid-webapp" {
   source        = "./modules/windows-webap"
-  webapp_name   = mid_webapi_name
+  webapp_name   = var.mid_webapi_name
   location      = module.rg.location
   rg_name       = module.rg.rg_name
   svc_plan_id   = module.mid-webapsvcplan.appsvc_plan_id
   public_access = false
 
 }
-
-# module "mid-webapp-pe" {
-#   source    = "./modules/private-endpoint"
-#   pe_name   = "mid-webapp-pe"
-#   location  = module.rg.location
-#   rg_name   = module.rg.rg_name
-#   subnet_id = module.subnet.snet_id[0]
-
-
-#   private_service_connection = {
-#     name                           = "midprivateendpointconnection"
-#     is_manual_connection           = false
-#     private_connection_resource_id = module.mid-webap.app_service_id
-#     subresource_names              = ["sites"]
-#   }
-#  private_dns_zone_group = {
-#     name                 = "privatednszonegroup"
-#     private_dns_zone_ids = [azurerm_private_dns_zone.dnsprivatezone.id]
-#  }
-
-# }
-
+#Middleware WebApp - Private Endpoint
 resource "azurerm_private_dns_zone" "dnsprivatezone" {
   name                = "privatelink.azurewebsites.net"
   resource_group_name = module.rg.rg_name
