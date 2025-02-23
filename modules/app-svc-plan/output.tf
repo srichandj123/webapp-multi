710ae942ec070fd1964bc6be20c62abbe0e373e2
srichandj123 <srichandbec@gmail.com>
2025-02-23 22:42:17 +0530

    Fixed the APSVCID

--- modules/app-svc-plan/output.tf
+++ modules/app-svc-plan/output.tf
@@ -1,4 +1,4 @@
 output "appsvc_plan_id" {
-  value       = azurerm_service_plan.app_svcplan.app_svcplan.id
+  value       = azurerm_service_plan.app_svcplan.id
   description = "The ID of the App SVC Plan"
 }
