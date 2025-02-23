17c368146814e9996618eddef5ac0168c4937a82
srichandj123 <srichandbec@gmail.com>
2025-02-23 20:17:47 +0530

    provider changes

--- providers.tf
+++ providers.tf
@@ -16,8 +16,5 @@ terraform {
 
 provider "azurerm" {
   features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false # This is to handle MCAPS or other policy driven resource creation.
-    }
   }
 }
