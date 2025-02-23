9758d8ecae70447cb08beb0c0c3466257499ec20
srichandj123 <srichandbec@gmail.com>
2025-02-23 21:49:17 +0530

    SQL DB

---
+++ azuresqldb.tf
@@ -0,0 +1,38 @@
+
+resource "azurerm_user_assigned_identity" "admin" {
+  name                = "sql-admin"
+  location            = module.rg.location
+  resource_group_name = module.rg.rg_name
+}
+
+resource "azurerm_mssql_server" "mssql" {
+  name                         = "prod-sqlserver"
+  resource_group_name          = module.rg.rg_name
+  location                     = module.rg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "mssqldb" {
+  name           = "prod-db"
+  server_id      = azurerm_mssql_server.mssql.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 100
+  read_scale     = true
+  sku_name       = "P2"
+  zone_redundant = true
+  tags           = module.rg.tags
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.admin.id]
+  }
+
+  transparent_data_encryption_key_vault_key_id = azurerm_key_vault_key.sqlkey.id
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ keyvault.tf
@@ -0,0 +1,36 @@
+
+# Create a key vault with access policies which allow for the current user to get, list, create, delete, update, recover, purge and getRotationPolicy for the key vault key and also add a key vault access policy for the Microsoft Sql Server instance User Managed Identity to get, wrap, and unwrap key(s)
+resource "azurerm_key_vault" "kv" {
+  name                        = "prodkv01"
+  location                    = module.rg.location
+  resource_group_name         = module.rg.rg_name
+  enabled_for_disk_encryption = true
+  tenant_id                   = azurerm_user_assigned_identity.admin.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = true
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = ["Get", "List", "Create", "Delete", "Update", "Recover", "Purge", "GetRotationPolicy"]
+  }
+
+  access_policy {
+    tenant_id = azurerm_user_assigned_identity.example.tenant_id
+    object_id = azurerm_user_assigned_identity.example.principal_id
+
+    key_permissions = ["Get", "WrapKey", "UnwrapKey"]
+  }
+}
+
+resource "azurerm_key_vault_key" "sqlkey" {
+  depends_on   = [azurerm_key_vault.kv]
+  name         = "sql-key"
+  key_vault_id = azurerm_key_vault.kv.id
+  key_type     = "RSA"
+  key_size     = 2048
+  key_opts     = ["unwrapKey", "wrapKey"]
+}
