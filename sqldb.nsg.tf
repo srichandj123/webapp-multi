50755dff37c6fda8c054e635f1a9f94884ce8837
srichandj123 <srichandbec@gmail.com>
2025-02-24 02:10:33 +0530

    SQL NSg

---
+++ sqldb.nsg.tf
@@ -0,0 +1,34 @@
+resource "azurerm_network_security_group" "sql_nsg" {
+  name                = "sql-nsg"
+  location            = module.rg.location
+  resource_group_name = module.rg.rg_name
+
+  security_rule {
+    name                       = "AllowWebSubnet"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = azurerm_private_endpoint.privateendpoint.private_ip_address
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "DenyAllInbound"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "sql_subnet_association" {
+  subnet_id                 = module.subnet.snet_id[1]
+  network_security_group_id = azurerm_network_security_group.sql_nsg.id
+}
