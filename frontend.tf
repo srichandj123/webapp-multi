0f4ce3c7274d3c52bc0789ddbb627e31cf96891b
srichandj123 <srichandbec@gmail.com>
2025-02-23 23:51:14 +0530

    VNET integration

--- frontend.tf
+++ frontend.tf
@@ -30,20 +30,8 @@ module "fe-webapp" {
 
 }
 
-# resource "azurerm_windows_web_app" "frontwebapp" {
-#   name                = "acme-corp-fe-webapi"
-#   location            = module.rg.location
-#   resource_group_name = module.rg.rg_name
-#   service_plan_id     = module.fe-webapsvcplan.appsvc_plan_id
 
-#   site_config {}
-#   app_settings = {
-#     # "WEBSITE_DNS_SERVER" : "168.63.129.16",
-#     # "WEBSITE_VNET_ROUTE_ALL" : "1"
-#   }
-# }
-
-# resource "azurerm_app_service_virtual_network_swift_connection" "vnetintegrationconnection" {
-#   app_service_id = module.fe-webapp.app_service_id
-#   subnet_id      = azurerm_subnet.front-integrationsubnet.id
-# }
+resource "azurerm_app_service_virtual_network_swift_connection" "vnetintegrationconnection" {
+  app_service_id = module.fe-webapp.app_service_id
+  subnet_id      = azurerm_subnet.front-integrationsubnet.id
+}
