87b67483ee0eca3e987e105aaac6fbfb0d0b1634
srichandj123 <srichandbec@gmail.com>
2025-02-24 02:05:17 +0530

    SQL PE point

--- sqldb-pe.tf
+++ sqldb-pe.tf
@@ -10,22 +10,16 @@ resource "azurerm_private_dns_zone_virtual_network_link" "dnszonelinksql" {
   virtual_network_id    = module.prodvnet.vnet_id
 }
 
-resource "azurerm_private_endpoint" "sqlprivateendpoint" {
-  name                = "sqldb-privateendpoint"
-  location            = module.rg.location
+resource "azurerm_private_endpoint" "sql" {
+  name                = "sql-private-endpoint"
   resource_group_name = module.rg.rg_name
+  location            = module.rg.location
   subnet_id           = module.subnet.snet_id[1]
 
-  private_dns_zone_group {
-    name                 = "sqlprivatednszonegroup"
-    private_dns_zone_ids = [azurerm_private_dns_zone.dnsprivatezonesql.id]
-  }
-
   private_service_connection {
-    name                           = "sqlprivateendpointconnection"
-    private_connection_resource_id = module.mid-webapp.app_service_id
+    name                           = "sql-privateserviceconnection"
+    private_connection_resource_id = azurerm_sql_server.mssql.id
     subresource_names              = ["sqlServer"]
     is_manual_connection           = false
   }
 }
-
